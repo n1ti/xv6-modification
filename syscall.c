94518db3a0334f7d5a9b278c0536b9fbdce0707f
Niti <115517964+n1ti@users.noreply.github.com>
2023-10-27 16:18:32 +0530

    Update syscall.c [adding history syscall]

--- syscall.c
+++ syscall.c
@@ -103,6 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_history(void);
 extern int sys_gpi(void);
 
 static int (*syscalls[])(void) = {
@@ -127,6 +128,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_history] sys_history,
 [SYS_gpi]     sys_gpi
 };
 
